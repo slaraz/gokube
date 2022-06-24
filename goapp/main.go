d138753ff409c51344ca437a85fed5026349c1e0
Sławomir Rażniewski <slawomir.razniewski@gmail.com>
2022-06-24 16:04:57 +0200

    koniec

--- goapp/main.go
+++ goapp/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
+	hn, _ := os.Hostname()
+	fmt.Fprintf(w, "%q: Cześć World! v4 %s", hn, time.Now())
 }
 
 func main() {
+	log.Println("Start.")
+	defer log.Println("Koniec.")
+
 	http.HandleFunc("/", greet)
-	http.ListenAndServe(":http", nil)
+	http.ListenAndServe(":http", loguj(http.DefaultServeMux))
+}
+
+func loguj(wewn http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		wewn.ServeHTTP(w, r)
+		log.Print(r.RequestURI, time.Since(start))
+	})
 }
